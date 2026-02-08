ea69ebbed36521b2536c4f8c879b4a67b7961b73
Ingrid <678103@stud.hvl.no>
2026-02-08 23:59:04 +0100

    Fjernet noe som ikke ble brukt

--- app/src/main/java/com/example/oblig1_quizapp/GalleryActivity.kt
+++ app/src/main/java/com/example/oblig1_quizapp/GalleryActivity.kt
@@ -3,8 +3,6 @@ package com.example.oblig1_quizapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 
 class GalleryActivity : ComponentActivity() {
 
